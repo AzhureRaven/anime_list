2c501e84c47231a2e924fbf4fe4f7ea50ece9761
AzhureRaven <abrahamarthurfendy@gmail.com>
2023-05-19 17:21:05 +0700

    Login Layout

--- lib/routes/account/login_screen.dart
+++ lib/routes/account/login_screen.dart
@@ -16,103 +16,132 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
-  GlobalKey<FormState> _formKey = GlobalKey <FormState>();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
   bool _passVisible = true;
+  int flex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
+    return Scaffold(body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            flex = 0;
+          } else if (constraints.maxWidth <= 1200) {
+            flex = 1;
+          } else {
+            flex = 2;
+          }
+      return SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
           child: Form(
             key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+            child: Row(
               children: [
-                const Text('Anime List', style: TextStyle(fontSize: 24)),
-                const SizedBox(height: 24),
-                const Text('Login into your account', style: TextStyle(fontSize: 18)),
-                const SizedBox(height: 24),
-                TextFormField(
-                  controller: _emailController,
-                  textInputAction: TextInputAction.next,
-                  validator: (data) {
-                    if (data != null && data != "") {
-                      return null;
-                    } else {
-                      return "Field is required";
-                    }
-                  },
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email'
-                  ),
+                Expanded(flex: flex, child: Row()),
+                Expanded(
+                  flex: 2,
+                  child: buildForm()
                 ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  controller: _passwordController,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: _passVisible,
-                  validator: (data) {
-                    if (data != null && data != "") {
-                      return null;
-                    } else {
-                      return "Field is required";
-                    }
-                  },
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: 'Password',
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _passVisible = !_passVisible;
-                        });
-                      },
-                      child: Icon(_passVisible ? Icons.visibility : Icons.visibility_off),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 8),
-                ElevatedButton(
-                  onPressed: () async{
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      context.loaderOverlay.show();
-                      try{
-                        final navigator = Navigator.of(context);
-                        final snackbar = ScaffoldMessenger.of(context);
-                        final email = _emailController.text;
-                        final password = _passwordController.text;
-                        await _auth.signInWithEmailAndPassword(email: email, password: password);
-                        navigator.pop();
-                        navigator.push(MaterialPageRoute(builder: (context){
-                          return HomeScreen();
-                        }));
-                        snackbar.showSnackBar(basicSnackBar("Login Succesful!"));
-                      }catch(e){
-                        ScaffoldMessenger.of(context).showSnackBar(basicSnackBar(e.toString()));
-                      }finally{
-                        context.loaderOverlay.hide();
-                      }
-                    }
-                  },
-                  child: const Text('Login', style: TextStyle(fontSize: 18)),
-                ),
-                TextButton(onPressed: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context){
-                    return RegisterScreen();
-                  }));
-                }, child: const Text("Don't have an account? Register Here!"))
+                Expanded(flex: flex, child: Row())
               ],
             ),
           ),
         ),
-      ),
+      );
+    }));
+  }
+
+  Widget buildForm(){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Text('Anime List', style: TextStyle(fontSize: 24)),
+        const SizedBox(height: 24),
+        const Text('Login into your account',
+            style: TextStyle(fontSize: 18)),
+        const SizedBox(height: 24),
+        TextFormField(
+          controller: _emailController,
+          textInputAction: TextInputAction.next,
+          validator: (data) {
+            if (data != null && data != "") {
+              return null;
+            } else {
+              return "Field is required";
+            }
+          },
+          decoration: const InputDecoration(
+              border: OutlineInputBorder(), labelText: 'Email'),
+        ),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: _passwordController,
+          keyboardType: TextInputType.visiblePassword,
+          obscureText: _passVisible,
+          validator: (data) {
+            if (data != null && data != "") {
+              return null;
+            } else {
+              return "Field is required";
+            }
+          },
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: 'Password',
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _passVisible = !_passVisible;
+                });
+              },
+              child: Icon(_passVisible
+                  ? Icons.visibility
+                  : Icons.visibility_off),
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        ElevatedButton(
+          onPressed: () async {
+            if (_formKey.currentState!.validate()) {
+              _formKey.currentState!.save();
+              context.loaderOverlay.show();
+              try {
+                final navigator = Navigator.of(context);
+                final snackbar = ScaffoldMessenger.of(context);
+                final email = _emailController.text;
+                final password = _passwordController.text;
+                await _auth.signInWithEmailAndPassword(
+                    email: email, password: password);
+                navigator.pop();
+                navigator.push(MaterialPageRoute(builder: (context) {
+                  return HomeScreen();
+                }));
+                snackbar
+                    .showSnackBar(basicSnackBar("Login Succesful!"));
+              } catch (e) {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(basicSnackBar(e.toString()));
+              } finally {
+                context.loaderOverlay.hide();
+              }
+            }
+          },
+          child: const Text('Login', style: TextStyle(fontSize: 18)),
+        ),
+        TextButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) {
+                return RegisterScreen();
+              }));
+            },
+            child: const Text("Don't have an account? Register Here!"))
+      ],
     );
   }
 
--- lib/routes/account/register_screen.dart
+++ lib/routes/account/register_screen.dart
@@ -19,123 +19,145 @@ class _RegisterScreenState extends State<RegisterScreen> {
   TextEditingController _confPassController = TextEditingController();
   bool _passVisible = true;
   bool _confPassVisible  = true;
+  int flex = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                const Text('Anime List', style: TextStyle(fontSize: 24)),
-                const SizedBox(height: 24),
-                const Text('Create your account', style: TextStyle(fontSize: 18)),
-                const SizedBox(height: 24),
-                TextFormField(
-                  controller: _emailController,
-                  textInputAction: TextInputAction.next,
-                  validator: (data) {
-                    if (data != null && data != "") {
-                      return null;
-                    } else {
-                      return "Field is required";
-                    }
-                  },
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email'
-                  ),
-                ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  controller: _passwordController,
-                  textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: _passVisible,
-                  validator: (data) {
-                    if (data != null && data != "") {
-                      return null;
-                    } else {
-                      return "Field is required";
-                    }
-                  },
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: 'Password',
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _passVisible = !_passVisible;
-                        });
-                      },
-                      child: Icon(_passVisible ? Icons.visibility : Icons.visibility_off),
+    return Scaffold(body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth <= 600) {
+            flex = 0;
+          } else if (constraints.maxWidth <= 1200) {
+            flex = 1;
+          } else {
+            flex = 2;
+          }
+          return SafeArea(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              child: Form(
+                key: _formKey,
+                child: Row(
+                  children: [
+                    Expanded(flex: flex, child: Row()),
+                    Expanded(
+                        flex: 2,
+                        child: buildForm()
                     ),
-                  ),
+                    Expanded(flex: flex, child: Row())
+                  ],
                 ),
-                const SizedBox(height: 8),
-                TextFormField(
-                  controller: _confPassController,
-                  obscureText: _confPassVisible,
-                  validator: (data) {
-                    if (data != null && data != "") {
-                      if(data != _passwordController.text){
-                        return "Confirm Password Invalid";
-                      }
-                      else{
-                        return null;
-                      }
-                    }
-                    else {
-                      return "Field is required";
-                    }
-                  },
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    labelText: "Confirm Password",
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _confPassVisible = !_confPassVisible;
-                        });
-                      },
-                      child: Icon(_confPassVisible ? Icons.visibility : Icons.visibility_off),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 8),
-                ElevatedButton(
-                  onPressed: () async{
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      context.loaderOverlay.show();
-                      try{
-                        final navigator = Navigator.of(context);
-                        final snackbar = ScaffoldMessenger.of(context);
-                        final email = _emailController.text;
-                        final password = _passwordController.text;
-                        await _auth.createUserWithEmailAndPassword(email: email, password: password);
-                        navigator.pop();
-                        snackbar.showSnackBar(basicSnackBar("Register Succesful!"));
-                      }catch(e){
-                        ScaffoldMessenger.of(context).showSnackBar(basicSnackBar(e.toString()));
-                      }finally{
-                        context.loaderOverlay.hide();
-                      }
-                    }
-                  },
-                  child: const Text('Register', style: TextStyle(fontSize: 18)),
-                ),
-                TextButton(onPressed: (){Navigator.pop(context);}, child: const Text("Already have an account? Login Here!"))
-              ],
+              ),
+            ),
+          );
+        }));
+  }
+
+  Widget buildForm(){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const Text('Anime List', style: TextStyle(fontSize: 24)),
+        const SizedBox(height: 24),
+        const Text('Create your account', style: TextStyle(fontSize: 18)),
+        const SizedBox(height: 24),
+        TextFormField(
+          controller: _emailController,
+          textInputAction: TextInputAction.next,
+          validator: (data) {
+            if (data != null && data != "") {
+              return null;
+            } else {
+              return "Field is required";
+            }
+          },
+          decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Email'
+          ),
+        ),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: _passwordController,
+          textInputAction: TextInputAction.next,
+          keyboardType: TextInputType.visiblePassword,
+          obscureText: _passVisible,
+          validator: (data) {
+            if (data != null && data != "") {
+              return null;
+            } else {
+              return "Field is required";
+            }
+          },
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: 'Password',
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _passVisible = !_passVisible;
+                });
+              },
+              child: Icon(_passVisible ? Icons.visibility : Icons.visibility_off),
             ),
           ),
         ),
-      ),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: _confPassController,
+          obscureText: _confPassVisible,
+          validator: (data) {
+            if (data != null && data != "") {
+              if(data != _passwordController.text){
+                return "Confirm Password Invalid";
+              }
+              else{
+                return null;
+              }
+            }
+            else {
+              return "Field is required";
+            }
+          },
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            labelText: "Confirm Password",
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _confPassVisible = !_confPassVisible;
+                });
+              },
+              child: Icon(_confPassVisible ? Icons.visibility : Icons.visibility_off),
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        ElevatedButton(
+          onPressed: () async{
+            if (_formKey.currentState!.validate()) {
+              _formKey.currentState!.save();
+              context.loaderOverlay.show();
+              try{
+                final navigator = Navigator.of(context);
+                final snackbar = ScaffoldMessenger.of(context);
+                final email = _emailController.text;
+                final password = _passwordController.text;
+                await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                navigator.pop();
+                snackbar.showSnackBar(basicSnackBar("Register Succesful!"));
+              }catch(e){
+                ScaffoldMessenger.of(context).showSnackBar(basicSnackBar(e.toString()));
+              }finally{
+                context.loaderOverlay.hide();
+              }
+            }
+          },
+          child: const Text('Register', style: TextStyle(fontSize: 18)),
+        ),
+        TextButton(onPressed: (){Navigator.pop(context);}, child: const Text("Already have an account? Login Here!"))
+      ],
     );
   }
 
